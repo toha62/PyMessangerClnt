9fcc5356ddf447b2d374c7d82966efeeebc0c060
Anton Trifonov <toha62@inbox.ru>
2021-09-04 23:25:42 +0300

    Add ui

--- main.py
+++ main.py
@@ -1,16 +1,19 @@
-# This is a sample Python script.
-
-# Press Shift+F10 to execute it or replace it with your code.
-# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
-
-
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
+import sys
+import datetime
+from PyQt6 import uic, QtCore, QtGui, QtWidgets
 
 
-# Press the green button in the gutter to run the script.
+class MainWindow(QtWidgets.QMainWindow):
+    ServerAdress = 'http://localhost:5000'
+    MessageID = 0
+
+    def __init__(self, *args, **kwargs):
+        super(MainWindow, self).__init__(*args, **kwargs)
+        uic.loadUi('msnger_cl.ui', self)
+
+
 if __name__ == '__main__':
-    print_hi('PyCharm')
-
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
+    app = QtWidgets.QApplication(sys.argv)
+    w = MainWindow()
+    w.show()
+    sys.exit(app.exec())
